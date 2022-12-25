ea6ad554ffd9bd32d07f64fa34fc0d169586cea6
Estiven Muñoz <estivenyeizon@gmail.com>
2022-12-25 15:08:51 -0500

    first

---
+++ JS/index.js
@@ -0,0 +1,7 @@
+function login(e){
+    console.log(e)
+    e.preventDefault()
+    let i = document.querySelector('lottie-player').classList.toggle('exit')
+    
+    // document.appendChild('')
+}
